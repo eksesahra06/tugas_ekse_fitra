bb370ec1e3d705b03077cf932266bcfb46b541dd
fitrafadila <dilafa516@gmail.com>
2023-05-17 10:29:58 +0700

    membuat index php

---
+++ index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <title>BIODATA</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+        <!-- Isi Biodata Paling Keren di HTML disini -->
+<div class="container">
+  <div class="card kartu">
+    <div class="row">
+      <div class="col-md-4">
+      <div class="foto">
+        <img src="foto/bayuafrizatulrizki.jpg" class="img-thumbnail" alt="" width="150" height="auto">
+      </div>
+      </div>
+      <div class="col-md-8 kertas-biodata">
+        <div class="biodata">
+        <table width="100%" border="0">
+    <tbody><tr>
+        <td valign="top">
+        <table>
+          <tbody>
+            <tr>
+                <td>Nama</td>
+                <td>:</td>
+                <td><input type="text"></td>
+            </tr>
+           <tr>
+                <td>Kelas</td>
+                <td>:</td>
+                <td><select name="" id="">
+                <option value="">11 RPL 1</option>
+                <option value="">11 RPL 2</option>
+                </select>
+                </td>
+            </tr>
+            <tr>
+                <td>Alamat</td>
+                <td>:</td>
+                <td><textarea name="" id="" cols="30" rows="10"></textarea></td>
+            </tr>
+        </tbody></table>
+        </td>
+    </tr>
+    </tbody></table>
+  </div>
+      </div>
+    </div>
+  </div>
+</div>
+  </body>
+</html>
